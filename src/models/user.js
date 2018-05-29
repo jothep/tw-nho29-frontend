48d88be28f6594899ee468aa6fa087542d0b7039
Edison Guo <hydra1983@gmail.com>
2018-05-29 21:47:50 +0800

    [fguo] 移除不需要的组件

--- src/models/user.js
+++ src/models/user.js
@@ -24,28 +24,4 @@ export default {
       });
     },
   },
-
-  reducers: {
-    save(state, action) {
-      return {
-        ...state,
-        list: action.payload,
-      };
-    },
-    saveCurrentUser(state, action) {
-      return {
-        ...state,
-        currentUser: action.payload,
-      };
-    },
-    changeNotifyCount(state, action) {
-      return {
-        ...state,
-        currentUser: {
-          ...state.currentUser,
-          notifyCount: action.payload,
-        },
-      };
-    },
-  },
 };
